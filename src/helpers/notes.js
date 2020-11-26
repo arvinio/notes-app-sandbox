ea5828090e5a43443fe85b04a14051dd66b2dace
Arvin Behshad <arvin@gimitheapp.com>
2020-11-26 03:36:12 +0100

    fixed empty notes

--- src/helpers/notes.js
+++ src/helpers/notes.js
@@ -1,24 +1,32 @@
+function initNotes() {
+  localStorage.setItem('notes', JSON.stringify([]))
+  return []
+}
+
 export function getNotes() {
-  const notes = localStorage.getItem("notes");
-  const parsedNotes = JSON.parse(notes);
-  return parsedNotes;
+  let notes = localStorage.getItem('notes')
+  if (!notes) {
+    notes = initNotes()
+  }
+  const parsedNotes = JSON.parse(notes)
+  return parsedNotes
 }
 
 export function createNote(title, priority) {
-  const notes = getNotes();
-  const newNote = { title, priority };
-  notes.push(newNote);
-  console.log("about to save", notes);
-  const jsonNoteArray = JSON.stringify(notes);
-  localStorage.setItem("notes", jsonNoteArray);
+  const notes = getNotes()
+  const newNote = { title, priority }
+  notes.push(newNote)
+  console.log('about to save', notes)
+  const jsonNoteArray = JSON.stringify(notes)
+  localStorage.setItem('notes', jsonNoteArray)
 }
 
 export function moveNote() {
-  const notes = getNotes();
-  const indexToMove = 0;
-  const noteToMove = notes.length > 0 ? notes[indexToMove] : null;
-  notes.splice(indexToMove, 1);
-  notes.splice(notes.length, 0, noteToMove);
-  const jsonNoteArray = JSON.stringify(notes);
-  localStorage.setItem("notes", jsonNoteArray);
+  const notes = getNotes()
+  const indexToMove = 0
+  const noteToMove = notes.length > 0 ? notes[indexToMove] : null
+  notes.splice(indexToMove, 1)
+  notes.splice(notes.length, 0, noteToMove)
+  const jsonNoteArray = JSON.stringify(notes)
+  localStorage.setItem('notes', jsonNoteArray)
 }
